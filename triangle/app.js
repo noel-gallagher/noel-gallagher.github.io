31414655bfccf395e53442ea5f6551dfe4ca541b
noel-gallagher <noel.gallagher164@gmail.com>
2023-09-27 20:36:35 +0100

    add webgl for lol

---
+++ triangle/app.js
@@ -0,0 +1,230 @@
+var camera = {
+  position: vec3.fromValues(0.0, 0.0, 0.0),
+  front: vec3.fromValues(0.0, 0.0, -1.0),
+  up: vec3.fromValues(0.0, 1.0, 0.0),
+  right: vec3.create(),
+  worldUp: vec3.fromValues(0.0, 1.0, 0.0),
+  yaw: -90.0,
+  pitch: 0.0,
+  movementSpeed: 0.1,
+  turnSpeed: 0.1,
+  update: function () {
+    this.front[0] =
+      Math.cos(glMatrix.toRadian(this.yaw)) *
+      Math.cos(glMatrix.toRadian(this.pitch));
+    this.front[1] = Math.sin(glMatrix.toRadian(this.pitch));
+    this.front[2] =
+      Math.sin(glMatrix.toRadian(this.yaw)) *
+      Math.cos(glMatrix.toRadian(this.pitch));
+    vec3.normalize(this.front, this.front);
+    vec3.cross(this.right, this.front, this.worldUp);
+    vec3.normalize(this.right, this.right);
+    vec3.cross(this.up, this.right, this.front);
+    vec3.normalize(this.up, this.up);
+  },
+  getViewMatrix: function () {
+    var lookAt = vec3.create();
+    vec3.add(lookAt, this.position, this.front);
+    return mat4.lookAt(mat4.create(), this.position, lookAt, this.up);
+  },
+  processKeyboard: function (direction, deltaTime) {
+    var velocity = this.movementSpeed * deltaTime;
+    if (direction === "forward")
+      this.position = vec3.add(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.front, velocity)
+      );
+    if (direction === "backward")
+      this.position = vec3.subtract(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.front, velocity)
+      );
+    if (direction === "left")
+      this.position = vec3.subtract(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.right, velocity)
+      );
+    if (direction === "right")
+      this.position = vec3.add(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.right, velocity)
+      );
+  },
+  processMouseMovement: function (deltaX, deltaY) {
+    this.yaw += this.turnSpeed * deltaX;
+    this.pitch += this.turnSpeed * deltaY;
+    if (this.pitch > 89.0) this.pitch = 89.0;
+    if (this.pitch < -89.0) this.pitch = -89.0;
+    this.update();
+  }
+};
+
+camera.update();
+
+var keys = {
+  forward: false,
+  backward: false,
+  left: false,
+  right: false
+};
+
+window.addEventListener("keydown", function (e) {
+  switch (e.code) {
+    case "KeyW":
+      keys.forward = true;
+      break;
+    case "KeyS":
+      keys.backward = true;
+      break;
+    case "KeyA":
+      keys.left = true;
+      break;
+    case "KeyD":
+      keys.right = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", function (e) {
+  switch (e.code) {
+    case "KeyW":
+      keys.forward = false;
+      break;
+    case "KeyS":
+      keys.backward = false;
+      break;
+    case "KeyA":
+      keys.left = false;
+      break;
+    case "KeyD":
+      keys.right = false;
+      break;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var canvas = document.getElementById("c");
+  var gl = canvas.getContext("webgl");
+
+  if (!gl) {
+    return;
+  }
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  var verSource = `
+        attribute vec3 pos;
+        varying vec4 vCol;
+        uniform mat4 model;
+        uniform mat4 projection;
+        uniform mat4 view;
+        void main() {
+            gl_Position = projection * view * model * vec4(pos, 1.0);
+            vCol = vec4(clamp(pos, 0.0, 1.0), 1.0);
+        }
+    `;
+
+  var fragSource = `
+        precision mediump float;
+        varying vec4 vCol;
+        void main() {
+            gl_FragColor = vCol;
+        }
+    `;
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, verSource);
+  gl.compileShader(vertexShader);
+
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragSource);
+  gl.compileShader(fragmentShader);
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  var vertices = new Float32Array([
+    -1.0,
+    -1.0,
+    0.0,
+    0.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.0
+  ]);
+
+  var indices = new Uint16Array([0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2]);
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  var posAttribLocation = gl.getAttribLocation(program, "pos");
+  gl.vertexAttribPointer(posAttribLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(posAttribLocation);
+
+  var modelUniform = gl.getUniformLocation(program, "model");
+  var projectionUniform = gl.getUniformLocation(program, "projection");
+
+  var viewMatrix = camera.getViewMatrix();
+  var viewUniform = gl.getUniformLocation(program, "view");
+  gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
+
+  var angle = 0.0;
+
+  function animate() {
+    angle += 0.01;
+    if (angle > 2 * Math.PI) angle = 0.0;
+
+    var modelMatrix = mat4.create();
+    mat4.translate(modelMatrix, modelMatrix, [0.0, 0.0, -2.5]);
+    mat4.scale(modelMatrix, modelMatrix, [0.4, 0.4, 0.4]);
+
+    var projectionMatrix = mat4.create();
+    mat4.perspective(
+      projectionMatrix,
+      Math.PI / 4,
+      canvas.width / canvas.height,
+      0.1,
+      100.0
+    );
+
+    gl.uniformMatrix4fv(modelUniform, false, modelMatrix);
+    gl.uniformMatrix4fv(projectionUniform, false, projectionMatrix);
+
+    if (keys.forward) camera.processKeyboard("forward", 0.1);
+    if (keys.backward) camera.processKeyboard("backward", 0.1);
+    if (keys.left) camera.processKeyboard("left", 0.1);
+    if (keys.right) camera.processKeyboard("right", 0.1);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    viewMatrix = camera.getViewMatrix();
+    gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
+
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
+
