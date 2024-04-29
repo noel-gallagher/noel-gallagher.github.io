0c5f9ff92b7994f2581ca417b1094c3460855e34
ng <noelgallagher164@gmail.com>
2024-04-29 21:14:02 +0100

    lint code

--- blog/observability.md
+++ blog/observability.md
@@ -3,8 +3,8 @@
 - the ability to understand a system by examining it’s outputs
 - metrics
 - logs
-- traces 
-faster troubleshooting
+- traces
+  faster troubleshooting
 
 ---
 
@@ -16,28 +16,33 @@ faster troubleshooting
 ---
 
 # What do we want to monitor?
-* thing 1 
-* thing 2
+
+- thing 1
+- thing 2
 
 ---
 
 # What do we want to alert on?
-* Thing 1
-* Thing 2
+
+- Thing 1
+- Thing 2
 
 ---
 
 # Logs
+
 Capturing detailed information
-* record events 
-* errors 
-* provide contextual data
+
+- record events
+- errors
+- provide contextual data
 
 ---
 
 # ELK stack
+
 - Elasticsearch for search
-- Logstash for collecting and aggregating logs 
+- Logstash for collecting and aggregating logs
 - Kibana for visualisation
 
 ---
@@ -53,9 +58,7 @@ For example: A user submits a request which travels through many backend service
 
 The request goes from A->B->C, each service A,B,C is an individual span, while a trace could be A->B or A->C or B-C
 
-
 ---
-
 
 # Grafana
 
@@ -77,30 +80,32 @@ collects and stores metrics as time series data
 
 ![prometheus architecture](https://prometheus.io/assets/architecture.png)
 
-
 ---
 
 # Core metrics:
 
-### counter: incremental 
-* example: number of requests
-* tasks completed
-* errors reported 
+### counter: incremental
+
+- example: number of requests
+- tasks completed
+- errors reported
 
 ### guage: increase/decrease (ie cpu usage, memory usage, dynamic data)
-* items in a queue
-* disk space used
+
+- items in a queue
+- disk space used
 
 ### histogram: distribution of values (ie latency P95, P98, P99)
-* request durations
-* response sizes`
+
+- request durations
+- response sizes`
 
 ---
 
 # Integration
+
 - Define what you want to expose (request counts, error rates, response times...)
-- start metric collection in application 
+- start metric collection in application
 - (most usecases) start http server which exposes these metrics
 - prometheus will scrape this endpoint
 - grafana will query promethus to display content
-
--- triangle/app.js
+++ triangle/app.js
@@ -20,275 +20,283 @@ var fragSource = `
        `;
 
 var camera = {
-   position: vec3.fromValues(0.0, 0.0, 0.0),
-   front: vec3.fromValues(0.0, 0.0, -1.0),
-   right: vec3.create(),
-   worldUp: vec3.fromValues(0.0, 1.0, 0.0),
-   yaw: -90.0,
-   pitch: 0.0,
-   movementSpeed: 0.1,
-   turnSpeed: 0.1,
-   
-   update: function () {
-       this.front[0] =
-           Math.cos(glMatrix.toRadian(this.yaw)) *
-           Math.cos(glMatrix.toRadian(this.pitch));
-       this.front[1] = Math.sin(glMatrix.toRadian(this.pitch));
-       this.front[2] =
-           Math.sin(glMatrix.toRadian(this.yaw)) *
-           Math.cos(glMatrix.toRadian(this.pitch));
-       vec3.normalize(this.front, this.front);
-       vec3.cross(this.right, this.front, this.worldUp);
-       vec3.normalize(this.right, this.right);
-       vec3.cross(this.worldUp, this.right, this.front);
-       vec3.normalize(this.worldUp, this.worldUp);
-       this.box.update();
-   },
-   
-   getViewMatrix: function () {
-       var lookAt = vec3.create();
-       vec3.add(lookAt, this.position, this.front);
-       return mat4.lookAt(mat4.create(), this.position, lookAt, this.worldUp);
+  position: vec3.fromValues(0.0, 0.0, 0.0),
+  front: vec3.fromValues(0.0, 0.0, -1.0),
+  right: vec3.create(),
+  worldUp: vec3.fromValues(0.0, 1.0, 0.0),
+  yaw: -90.0,
+  pitch: 0.0,
+  movementSpeed: 0.1,
+  turnSpeed: 0.1,
+
+  update: function () {
+    this.front[0] =
+      Math.cos(glMatrix.toRadian(this.yaw)) *
+      Math.cos(glMatrix.toRadian(this.pitch));
+    this.front[1] = Math.sin(glMatrix.toRadian(this.pitch));
+    this.front[2] =
+      Math.sin(glMatrix.toRadian(this.yaw)) *
+      Math.cos(glMatrix.toRadian(this.pitch));
+    vec3.normalize(this.front, this.front);
+    vec3.cross(this.right, this.front, this.worldUp);
+    vec3.normalize(this.right, this.right);
+    vec3.cross(this.worldUp, this.right, this.front);
+    vec3.normalize(this.worldUp, this.worldUp);
+    this.box.update();
+  },
+
+  getViewMatrix: function () {
+    var lookAt = vec3.create();
+    vec3.add(lookAt, this.position, this.front);
+    return mat4.lookAt(mat4.create(), this.position, lookAt, this.worldUp);
+  },
+
+  processKeyboard: function (direction, deltaTime) {
+    var velocity = this.movementSpeed * deltaTime;
+
+    if (direction === "forward")
+      this.position = vec3.add(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.front, velocity),
+      );
+    if (direction === "backward")
+      this.position = vec3.subtract(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.front, velocity),
+      );
+    if (direction === "left")
+      this.position = vec3.subtract(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.right, velocity),
+      );
+    if (direction === "right")
+      this.position = vec3.add(
+        vec3.create(),
+        this.position,
+        vec3.scale(vec3.create(), this.right, velocity),
+      );
+  },
+
+  processMouseMovement: function (deltaX, deltaY) {
+    this.yaw += this.turnSpeed * deltaX;
+    this.pitch += this.turnSpeed * deltaY;
+    if (this.pitch > 89.0) this.pitch = 89.0;
+    if (this.pitch < -89.0) this.pitch = -89.0;
+    this.update();
+  },
+
+  box: {
+    min: vec3.create(),
+    max: vec3.create(),
+    update: function () {
+      this.min = vec3.fromValues(
+        camera.position[0] - 0.1,
+        camera.position[1] - 0.1,
+        camera.position[2] - 0.1,
+      );
+      this.max = vec3.fromValues(
+        camera.position[0] + 0.1,
+        camera.position[1] + 0.1,
+        camera.position[2] + 0.1,
+      );
     },
-
-   processKeyboard: function (direction, deltaTime) {
-       var velocity = this.movementSpeed * deltaTime;
-
-       if (direction === "forward")
-           this.position = vec3.add(
-           vec3.create(),
-           this.position,
-           vec3.scale(vec3.create(), this.front, velocity)
-       );
-       if (direction === "backward")
-           this.position = vec3.subtract(
-           vec3.create(),
-           this.position,
-           vec3.scale(vec3.create(), this.front, velocity)
-       );
-       if (direction === "left")
-           this.position = vec3.subtract(
-           vec3.create(),
-           this.position,
-           vec3.scale(vec3.create(), this.right, velocity)
-       );
-       if (direction === "right")
-           this.position = vec3.add(
-           vec3.create(),
-           this.position,
-           vec3.scale(vec3.create(), this.right, velocity)
-       );
-   },
-
-   processMouseMovement: function (deltaX, deltaY) {
-       this.yaw += this.turnSpeed * deltaX;
-       this.pitch += this.turnSpeed * deltaY;
-       if (this.pitch > 89.0) this.pitch = 89.0;
-       if (this.pitch < -89.0) this.pitch = -89.0;
-       this.update();
-   },
-
-   box: {
-       min: vec3.create(),
-       max: vec3.create(),
-       update: function() {
-           this.min = vec3.fromValues(camera.position[0] - 0.1, camera.position[1] - 0.1, camera.position[2] - 0.1);
-           this.max = vec3.fromValues(camera.position[0] + 0.1, camera.position[1] + 0.1, camera.position[2] + 0.1);
-       }
-   }
+  },
 };
 
-
 var keys = {
-   forward: false,
-   backward: false,
-   left: false,
-   right: false
+  forward: false,
+  backward: false,
+  left: false,
+  right: false,
 };
 
 window.addEventListener("keydown", function (e) {
-   switch (e.code) {
-       case "KeyW":
-           keys.forward = true;
-           break;
-       case "KeyS":
-           keys.backward = true;
-           break;
-       case "KeyA":
-           keys.left = true;
-           break;
-       case "KeyD":
-           keys.right = true;
-           break;
+  switch (e.code) {
+    case "KeyW":
+      keys.forward = true;
+      break;
+    case "KeyS":
+      keys.backward = true;
+      break;
+    case "KeyA":
+      keys.left = true;
+      break;
+    case "KeyD":
+      keys.right = true;
+      break;
   }
 });
 
-
 window.addEventListener("keyup", function (e) {
-   switch (e.code) {
-       case "KeyW":
-           keys.forward = false;
-           break;
-       case "KeyS":
-           keys.backward = false;
-           break;
-       case "KeyA":
-           keys.left = false;
-           break;
-       case "KeyD":
-           keys.right = false;
-           break;
+  switch (e.code) {
+    case "KeyW":
+      keys.forward = false;
+      break;
+    case "KeyS":
+      keys.backward = false;
+      break;
+    case "KeyA":
+      keys.left = false;
+      break;
+    case "KeyD":
+      keys.right = false;
+      break;
   }
 });
 
 function updateCameraView(e) {
-    var movementX = e.movementX || e.mozMovementX || 0;
-    var movementY = e.movementY || e.mozMovementY || 0;
-    camera.processMouseMovement(movementX, -movementY);
+  var movementX = e.movementX || e.mozMovementX || 0;
+  var movementY = e.movementY || e.mozMovementY || 0;
+  camera.processMouseMovement(movementX, -movementY);
 }
 
 function isColliding(box1, box2) {
-    return (box1.min[0] <= box2.max[0] && box1.max[0] >= box2.min[0]) &&
-           (box1.min[1] <= box2.max[1] && box1.max[1] >= box2.min[1]) &&
-           (box1.min[2] <= box2.max[2] && box1.max[2] >= box2.min[2]);
+  return (
+    box1.min[0] <= box2.max[0] &&
+    box1.max[0] >= box2.min[0] &&
+    box1.min[1] <= box2.max[1] &&
+    box1.max[1] >= box2.min[1] &&
+    box1.min[2] <= box2.max[2] &&
+    box1.max[2] >= box2.min[2]
+  );
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-   var canvas = document.getElementById("c");
-   var gl = canvas.getContext("webgl");
-   
+  var canvas = document.getElementById("c");
+  var gl = canvas.getContext("webgl");
 
-   canvas.addEventListener('click', function () {
-       canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
-       canvas.requestPointerLock();
-   });
- 
-   function lockChangeAlert() {
-       if (document.pointerLockElement === canvas || document.mozPointerLockElement === canvas) {
-           document.addEventListener("mousemove", updateCameraView, false);
-       } else {
-           document.removeEventListener("mousemove", updateCameraView, false);
-       } 
-   }
+  canvas.addEventListener("click", function () {
+    canvas.requestPointerLock =
+      canvas.requestPointerLock || canvas.mozRequestPointerLock;
+    canvas.requestPointerLock();
+  });
 
-   document.addEventListener('pointerlockchange', lockChangeAlert, false);
-   document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
-   camera.update();
-
-   if (!gl) {
-       return;
-   }
-
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-   gl.viewport(0, 0, canvas.width, canvas.height);
-
-   var mouse = {
-   lastX: canvas.width / 2,
-   lastY: canvas.height / 2,
-   offsetX: 0,
-   offsetY: 0,
-};
-
-canvas.addEventListener('mousemove', function (e) {
-   var x = e.clientX;
-   var y = e.clientY;
-
-   mouse.offsetX = x - mouse.lastX;
-   mouse.offsetY = mouse.lastY - y; 
-
-   mouse.lastX = x;
-   mouse.lastY = y;
-
-   camera.processMouseMovement(mouse.offsetX, mouse.offsetY);
-});
-
-   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-   gl.shaderSource(vertexShader, verSource);
-   gl.compileShader(vertexShader);
-
-   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-   gl.shaderSource(fragmentShader, fragSource);
-   gl.compileShader(fragmentShader);
-
-   var program = gl.createProgram();
-   gl.attachShader(program, vertexShader);
-   gl.attachShader(program, fragmentShader);
-   gl.linkProgram(program);
-   gl.useProgram(program);
-
-   var vertices = new Float32Array([
-       -1.0, -1.0, 0.0,
-       0.0, -1.0, 1.0,
-       1.0, -1.0, 0.0,
-       0.0, 1.0, 0.0
-  ]);
-   var pyramidBox = {
-       min: vec3.fromValues(-0.4, -0.4, -3.1),
-       max: vec3.fromValues(0.4, 0.4, -2.1)
-   };
-   var indices = new Uint16Array([0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2]);
-   var vertexBuffer = gl.createBuffer();
-   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-   var indexBuffer = gl.createBuffer();
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-   var posAttribLocation = gl.getAttribLocation(program, "pos");
-   gl.vertexAttribPointer(posAttribLocation, 3, gl.FLOAT, false, 0, 0);
-   gl.enableVertexAttribArray(posAttribLocation);
-
-   var modelUniform = gl.getUniformLocation(program, "model");
-   var projectionUniform = gl.getUniformLocation(program, "projection");
- 
-   var viewMatrix = camera.getViewMatrix();
-   var viewUniform = gl.getUniformLocation(program, "view");
-   gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
-
-
-   function animate() {
-
-       var modelMatrix = mat4.create();
-       mat4.translate(modelMatrix, modelMatrix, [0.0, 0.0, -2.5]);
-       mat4.scale(modelMatrix, modelMatrix, [0.4, 0.4, 0.4]);
-
-       var projectionMatrix = mat4.create();
-       mat4.perspective(
-           projectionMatrix,
-           Math.PI / 4,
-           canvas.width / canvas.height,
-           0.1,
-           100.0
-       );
-
-   gl.uniformMatrix4fv(modelUniform, false, modelMatrix);
-   gl.uniformMatrix4fv(projectionUniform, false, projectionMatrix);
-
-   let previousPosition = vec3.clone(camera.position);
-
-   if (keys.forward) camera.processKeyboard("forward", 0.1);
-   if (keys.backward) camera.processKeyboard("backward", 0.1);
-   if (keys.left) camera.processKeyboard("left", 0.1);
-   if (keys.right) camera.processKeyboard("right", 0.1);
-
-   camera.box.update();
-   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-   gl.enable(gl.DEPTH_TEST);
-
-   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-
-   viewMatrix = camera.getViewMatrix();
-   gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
-
-   if(isColliding(camera.box, pyramidBox)) {
-       camera.position = previousPosition;
+  function lockChangeAlert() {
+    if (
+      document.pointerLockElement === canvas ||
+      document.mozPointerLockElement === canvas
+    ) {
+      document.addEventListener("mousemove", updateCameraView, false);
+    } else {
+      document.removeEventListener("mousemove", updateCameraView, false);
     }
-   
-   requestAnimationFrame(animate);
-}
-   animate();
-});
+  }
 
+  document.addEventListener("pointerlockchange", lockChangeAlert, false);
+  document.addEventListener("mozpointerlockchange", lockChangeAlert, false);
+  camera.update();
+
+  if (!gl) {
+    return;
+  }
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  var mouse = {
+    lastX: canvas.width / 2,
+    lastY: canvas.height / 2,
+    offsetX: 0,
+    offsetY: 0,
+  };
+
+  canvas.addEventListener("mousemove", function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+
+    mouse.offsetX = x - mouse.lastX;
+    mouse.offsetY = mouse.lastY - y;
+
+    mouse.lastX = x;
+    mouse.lastY = y;
+
+    camera.processMouseMovement(mouse.offsetX, mouse.offsetY);
+  });
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, verSource);
+  gl.compileShader(vertexShader);
+
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragSource);
+  gl.compileShader(fragmentShader);
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  var vertices = new Float32Array([
+    -1.0, -1.0, 0.0, 0.0, -1.0, 1.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0,
+  ]);
+  var pyramidBox = {
+    min: vec3.fromValues(-0.4, -0.4, -3.1),
+    max: vec3.fromValues(0.4, 0.4, -2.1),
+  };
+  var indices = new Uint16Array([0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2]);
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  var posAttribLocation = gl.getAttribLocation(program, "pos");
+  gl.vertexAttribPointer(posAttribLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(posAttribLocation);
+
+  var modelUniform = gl.getUniformLocation(program, "model");
+  var projectionUniform = gl.getUniformLocation(program, "projection");
+
+  var viewMatrix = camera.getViewMatrix();
+  var viewUniform = gl.getUniformLocation(program, "view");
+  gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
+
+  function animate() {
+    var modelMatrix = mat4.create();
+    mat4.translate(modelMatrix, modelMatrix, [0.0, 0.0, -2.5]);
+    mat4.scale(modelMatrix, modelMatrix, [0.4, 0.4, 0.4]);
+
+    var projectionMatrix = mat4.create();
+    mat4.perspective(
+      projectionMatrix,
+      Math.PI / 4,
+      canvas.width / canvas.height,
+      0.1,
+      100.0,
+    );
+
+    gl.uniformMatrix4fv(modelUniform, false, modelMatrix);
+    gl.uniformMatrix4fv(projectionUniform, false, projectionMatrix);
+
+    let previousPosition = vec3.clone(camera.position);
+
+    if (keys.forward) camera.processKeyboard("forward", 0.1);
+    if (keys.backward) camera.processKeyboard("backward", 0.1);
+    if (keys.left) camera.processKeyboard("left", 0.1);
+    if (keys.right) camera.processKeyboard("right", 0.1);
+
+    camera.box.update();
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    viewMatrix = camera.getViewMatrix();
+    gl.uniformMatrix4fv(viewUniform, false, viewMatrix);
+
+    if (isColliding(camera.box, pyramidBox)) {
+      camera.position = previousPosition;
+    }
+
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
